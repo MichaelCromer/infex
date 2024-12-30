9146ecfbfcd350fd46ecede5b97da80122ecace6
Michael Cromer <mcromer@proton.me>
2024-12-30 20:42:41 +1100

    Put world data at file scope

--- src/world.c
+++ src/world.c
@@ -2,56 +2,41 @@
 
 #include "include/world.h"
 
+#define MAX_U (64)
+#define MAX_V (64)
+#define MAX_TILES ((MAX_U) * (MAX_V))
+#define UV_SCALE (128)
 #define ROOT_3 (1.7320508f)
 #define ROOT_3_INV (0.5773502f)
-#define UV_SCALE (100)
 #define DELTA_U (UV_SCALE * ROOT_3)
 #define DELTA_V (UV_SCALE / 2.0f)
 
-/* is_null is a guarantee that all the data fields are blank and/or NULLed */
-bool is_null = true;
+/* scalar function map data */
+Vector2 centres[MAX_TILES] = { 0 }; /* geometric locations on-screen of tile centres */
+uint8_t heights[MAX_TILES] = { 0 }; /* heightmap of tiles */
+float enemy[MAX_TILES] = { 0 };     /* heightmap of enemy growth */
 
-/* basic grid geometry */
+/* current state */
 size_t num_tiles = 0;
 size_t num_rows = 0,
        num_cols = 0;
-Vector2 *centres = NULL; /* geometric locations on-screen of tile centres */
-
-/* scalar function map data */
-uint8_t *heights = NULL; /* heightmap of tiles */
-float *enemy = NULL;     /* heightmap of enemy growth */
 
 void world_initialise(size_t rows, size_t cols)
 {
-    if (!is_null) return;
-
     num_rows = rows, num_cols = cols;
     num_tiles = rows * cols;
 
-    heights = malloc(num_tiles * sizeof(uint8_t));
-    memset(heights, 0, num_tiles);
-
-    int u = 0, v = 0;
-    centres = malloc(num_tiles * sizeof(Vector2));
+    float u = 0, v = 0;
     Vector2 *ptr = centres;
     for (size_t r = 0; r < rows; r++) {
+        u = (r % 2) ? (DELTA_U / 2) : 0.0f;
         for (size_t c = 0; c < cols; c++) {
             *ptr = (Vector2) {u, v};
             u += DELTA_U;
             ptr++;
         }
         v += DELTA_V;
-        if (r % 2) {
-            u -= DELTA_U;
-        }
     }
-
-    is_null = false;
-}
-
-bool world_is_null(void)
-{
-    return is_null;
 }
 
 size_t world_num_tiles(void)
